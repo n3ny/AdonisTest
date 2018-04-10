1e6604def28afe0ac981cb27019ff738390e9e91
n3ny <n3nyrbd@gmail.com>
2018-04-10 09:27:53 -0500

    Actualizando la migración del empleado

--- NullDataAdonisJs/database/migrations/1521522138091_empleado_schema.js
+++ NullDataAdonisJs/database/migrations/1521522138091_empleado_schema.js
@@ -4,15 +4,24 @@ const Schema = use('Schema')
 
 class EmpleadoSchema extends Schema {
   up () {
-    this.table('empleados', (table) => {
-      // alter table
+    this.create('empleados', (table) => {
+      table.increments()
+      table.string('nombre', 50).notNullable()
+      table.string('apellido_p', 50).notNullable()
+      table.string('apellido_m', 50)
+      table.date('fecha_nac', 80).notNullable()
+      table.string('email', 254).notNullable().unique()
+      table.string('calle', 60).notNullable()
+      table.string('numero', 50).notNullable()
+      table.string('colonia', 254).notNullable()
+      table.string('estado', 60).notNullable()
+      table.string('pais', 80).notNullable()
+      table.timestamps()
     })
   }
 
   down () {
-    this.table('empleados', (table) => {
-      // reverse alternations
-    })
+    this.drop('empleados')
   }
 }
 
